70ddc87bd989401b5517499a7e4e10417a04fde1
Kellie Kumasaka <kelliekumasaka@Kellies-MBP.lan>
2024-06-24 18:12:03 -0700

    copying msw to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteStaticCopy({
+    targets: [
+      {
+        src: 'src/mockServiceWorker.js', // Adjust this path to your actual file location
+        dest: '' // Copy to the root of the dist directory
+      }
+    ]
+  })],
   server: {
     open: true,
   },
